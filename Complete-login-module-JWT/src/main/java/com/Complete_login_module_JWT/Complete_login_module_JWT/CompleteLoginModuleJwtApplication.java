f3fd38a197ecc79a68e5e66380c2b89db4cf04c1
adityathodsare <thodsareaditya@gmail.com>
2025-04-30 10:22:28 +0530

    Your message describing the changes

--- Complete-login-module-JWT/src/main/java/com/Complete_login_module_JWT/Complete_login_module_JWT/CompleteLoginModuleJwtApplication.java
+++ Complete-login-module-JWT/src/main/java/com/Complete_login_module_JWT/Complete_login_module_JWT/CompleteLoginModuleJwtApplication.java
@@ -9,5 +9,10 @@ public class CompleteLoginModuleJwtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CompleteLoginModuleJwtApplication.class, args);
 	}
+	/*
+	git add .
+	git commit -m "Your message describing the changes"
+	git push
+	 */
 
 }
